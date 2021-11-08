7958107539e57bbca05339544cc76692577406e1
ThiagoRovetta <rovetta.thiago@gmail.com>
2021-11-08 00:23:41 -0300

    Created Header component.

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
 
+import { Header } from "./Header";
 import { MovieCard } from "./MovieCard";
 
 import '../styles/content.scss';
@@ -43,14 +44,18 @@ export function Content({ selectedGenreId }: ContentProps) {
 
   return (
     <div className="container">
-      <header>
-        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-      </header>
+      <Header title={selectedGenre.title} />
 
       <main>
         <div className="movies-list">
           {movies.map(movie => (
-            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            <MovieCard 
+              key={movie.imdbID} 
+              title={movie.Title} 
+              poster={movie.Poster} 
+              runtime={movie.Runtime} 
+              rating={movie.Ratings[0].Value} 
+            />
           ))}
         </div>
       </main>
